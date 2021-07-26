a60dc0e23fde9ea2a1e40aefad032a7e82975019
dg410 <61630209+dg410@users.noreply.github.com>
2021-07-26 20:08:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ import requests
 app = Flask(__name__)
 
 
-@app.route('/clients')
-def get_splunk():
+@app.route('/clients/<span>')
+def get_splunk(span):
+    if span not in ["1mon", "3mon", "6mon"]:
+        return "{'error': 'Wrong time span'}"
     url = "https://40.71.31.225:8089/services/search/jobs"
-
-    payload = 'search=search%20index%3D_internal&exec_mode=oneshot&output_mode=json&earliest=-1h'
+    payload = 'search=search%20index%3D_internal&exec_mode=oneshot&output_mode=json&earliest=-{}'.format(span)
     headers = {
         'Authorization': 'Basic YWRtaW46QWRtaW4xMjM0NTY=',
         'Content-Type': 'application/x-www-form-urlencoded'
