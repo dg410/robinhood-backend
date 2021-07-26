1224f4c5d4be88868ba9c4ee830f44a949b5932b
dg410 <61630209+dg410@users.noreply.github.com>
2021-07-26 18:49:53 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/splunk')
+def get_splunk():
+    url = "https://40.71.31.225:8089/services/search/jobs"
+
+    payload = 'search=search%20index%3D_internal&exec_mode=oneshot&output_mode=json&earliest=-1h'
+    headers = {
+        'Authorization': 'Basic YWRtaW46QWRtaW4xMjM0NTY=',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    response = requests.request("POST", url, headers=headers, verify=False, data=payload)
+    return(response.text)
+
+
+if __name__ == '__main__':
+    app.run()
