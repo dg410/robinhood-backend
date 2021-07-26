500ea21a5fee6abadfbd40040d02bc884807878a
dg410 <61630209+dg410@users.noreply.github.com>
2021-07-26 23:56:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,17 @@
 from flask import Flask
-import requests
 import request_quota
-
+import request_splunk
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/clients/<span>')
-def get_splunk(span):
+@app.route('/clients/<search_type>/<span>')
+def get_stats(search_type, span):
     if span not in ["1mon", "3mon", "6mon"]:
         return "{'error': 'Wrong time span'}"
-    url = "https://40.71.31.225:8089/services/search/jobs"
-    payload = 'search=search%20index%3D_internal&exec_mode=oneshot&output_mode=json&earliest=-{}'.format(span)
-    headers = {
-        'Authorization': 'Basic YWRtaW46QWRtaW4xMjM0NTY=',
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
+    return json.dumps(request_splunk.splunk(search_type, span))
 
-    response = requests.request("POST", url, headers=headers, verify=False, data=payload)
-    return(response.text)
 
 
 @app.route('/quota/<namespace>/<cpu>/<ram>')
