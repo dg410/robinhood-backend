e9120e0ce677e46ade76bccc9f196eae86dcedc1
dg410 <61630209+dg410@users.noreply.github.com>
2021-07-27 03:22:19 +0300

    Update request_splunk.py

--- request_splunk.py
+++ request_splunk.py
@@ -3,7 +3,7 @@ import requests
 
 def splunk(namespace, span):
     url = "https://40.71.31.225:8089/services/search/jobs"
-    search_stats = "search=search%20source%3D%22text.txt%22%20host%3D%22splunk-hackaton%22%20index%3D%22ocp-data%22%20sourcetype%3D%22aaa%22%20namespace%3D{}%7Cstats%20perc90(ram)%20perc90(ram_request)%20max(ram)%20values(ram_quota)%20as%20ram_quota".format(namespace)
+    search_stats = "search=search%20source%3D%22text.txt%22%20host%3D%22splunk-hackaton%22%20index%3D%22ocp-data%22%20sourcetype%3D%22aaa%22%20namespace%3D{}%7Cstats%20perc90(ram)%20as%20perc99ram%20perc90(ram_request)%20as%20perc99ramreq%20max(ram)%20as%20maxram%20values(ram_quota)%20as%20ram_quota".format(namespace)
     search_data = 'search=search%20source%3D%22text.txt%22%20host%3D%22splunk-hackaton%22%20index%3D%22ocp-data%22%20sourcetype%3D%22aaa%22%20namespace%3D{}%7C%20timechart%20span%3D1h%20values(ram)%20as%20ram%20values(cpu)%20as%20cpu%20values(ram_quota)%20as%20ram-quota%20values(cpu_quota)%20as%20cpu-quota'.format(namespace)
     results = []
     for search in [search_stats, search_data]:
