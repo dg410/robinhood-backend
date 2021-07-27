6bb031f1ca09f47f6dfcc8cdac9292e2fb5f5d9c
dg410 <61630209+dg410@users.noreply.github.com>
2021-07-27 03:24:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import request_quota
 import request_splunk
+import json
 
 
 app = Flask(__name__)
@@ -10,22 +11,22 @@ app = Flask(__name__)
 def get_stats(span):
     if span not in ["1mon", "3mon", "6mon"]:
         return "{'error': 'Wrong time span'}"
-    results = {}
+    results = []
     for namespace in ["poor_project", "rich_project"]:
-        results[namespace] = request_splunk.splunk(namespace, span)
-    return results
-
-
-
-@app.route('/quota/<namespace>/<cpu>/<ram>')
-def change_quota(namespace, cpu, ram):
-    return request_quota.patch_quota(namespace, cpu, ram)
+        results.append({"namespace": namespace, "results": request_splunk.splunk(namespace, span)})
+    return json.dumps(results)
 
 
 @app.after_request
 def apply_caching(response):
     response.headers["Access-Control-Allow-Origin"] = "*"
     return response
+
+
+
+@app.route('/quota/<namespace>/<cpu>/<ram>')
+def change_quota(namespace, cpu, ram):
+    return request_quota.patch_quota(namespace, cpu, ram)
 
 
 if __name__ == '__main__':
